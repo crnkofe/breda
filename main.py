18947f2aa93f5be460814d04bda11b17eba51682
Simon Mihevc <simon.mihevc@gmail.com>
2017-07-10 23:04:27 +0200

    Update main.py

--- main.py
+++ main.py
@@ -149,4 +149,4 @@ def slack_random(u, c, m):
     else:
         return _pic('http://www.babel.crackerboxpalace.com/gifs/strangelove-wat.gif')
 
-app.run(host='0.0.0.0', port=PORT)
+#app.run(host='0.0.0.0', port=PORT)
